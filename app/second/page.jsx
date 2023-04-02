b54ecc572b3ac9f08059a2eea4af77010f55e7d3
Sebastian Medina <snmedina91@gmail.com>
2023-04-02 14:40:37 -0300

    add fetch

--- app/second/page.jsx
+++ app/second/page.jsx
@@ -1,5 +1,24 @@
-import Link from 'next/link'
-
-export default function Page() {
-    return (<Link href="/breaking">navigate to breaking</Link>);
+async function getData() {
+    // Get the total number of people
+    const res = await fetch('https://swapi.dev/api/people/');
+    const data = await res.json();
+    const total = data.count;
+  
+    // Get a random person
+    const index = Math.floor(Math.random() * total) + 1;
+    const res2 = await fetch(`https://swapi.dev/api/people/${index}`);
+    const data2 = await res2.json();
+  
+    return data2;
+  }
+  
+  export default async function Page() {
+    const data = await getData();
+    console.log(data)
+    const randomPerson =  data.name;
+    return (
+      <p>
+        {randomPerson}
+      </p>
+    );
   }
